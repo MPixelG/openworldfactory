8ae945efe42177f7c5497c814771e396ff9459fd
FrediMerz <fredimerz@outlook.de>
2026-05-07 18:20:08 +0200

    Reimplemented Cubesphere and Octasphere now working (chainsaws where originally invented for child birth -random facts with LetzTiz #2)

--- Assets/TerrainFace.cs
+++ Assets/TerrainFace.cs
@@ -2,27 +2,43 @@ using UnityEngine;
 
 public class TerrainFace
 {
+    bool _cubeMesh;
     Mesh _mesh;
     int _resolution;
     private Vector3[] _vertices;
+    Vector3 _localUp;
+    Vector3 _axisA;
+    Vector3 _axisB;
 
+    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    {
+        this._cubeMesh = true;
+        this._localUp =  localUp;
+        this._mesh = mesh;
+        this._resolution = resolution;
+        _axisA = new Vector3(localUp.y, localUp.z, localUp.x);
+        _axisB = Vector3.Cross(localUp, _axisA);
+    }
     public TerrainFace(Mesh mesh, int resolution, Vector3[] vertices)
     {
+        this._cubeMesh = false;
+        this._localUp = new Vector3(0, 0, 0);
         this._mesh = mesh;
         this._resolution = resolution;
         this._vertices = vertices;
-        
+        _axisA = new Vector3(_localUp.y, _localUp.z, _localUp.x);
+        _axisB = Vector3.Cross(_localUp, _axisA);
     }
     public (Vector3[], int[]) TriangleFragmentation(Vector3[] triangle, int res)
     {
         Vector3[] vertices = new Vector3[(res + 1)*(res + 2) / 2];
         int[] triangles = new int[res * res*3];
-        vertices[0] = triangle[0];
+        vertices[0] = triangle[2];
         vertices[((res+1)*(res+2)/2)-1] = triangle[1];
-        vertices[res*(res+1)/2] = triangle[2];
-        Vector3 u = triangle[0];
+        vertices[res*(res+1)/2] = triangle[0];
+        Vector3 u = triangle[2];
         Vector3 r = triangle[1];
-        Vector3 l = triangle[2];
+        Vector3 l = triangle[0];
         int n = 0;
         int m = 0;
         for (int i = 1; i < res; i++)
@@ -43,7 +59,7 @@ public class TerrainFace
 
         int n2 = 1;
         int triCount = 0;
-        for (int i = 0; i < (res * (res - 1) / 2) - 1; i++)
+        for (int i = 0; i < ((res) * (res + 1) / 2); i++)
         {
             triangles[triCount] = i;
             triangles[triCount + 1] = i + n2+1;
@@ -64,9 +80,9 @@ public class TerrainFace
         return (vertices, triangles);
     }
 
-    public void ConstructMesh()
+    public (Vector3[],int[]) ConstructCubeMesh()
     {
-        /*Vector3[] vertices = new Vector3[_resolution * _resolution];
+        Vector3[] vertices = new Vector3[_resolution * _resolution];
         int[] triangles = new int[(_resolution-1) * (_resolution-1) * 6];
         int triIndex = 0;
         for (int y = 0; y < _resolution; y++)
@@ -89,13 +105,34 @@ public class TerrainFace
                     triIndex += 6;
                 }
             }
-        }*/
-        (Vector3[],int[]) fragmentedTriangles = TriangleFragmentation(_vertices, _resolution);
-        Vector3[] pointOnOctahedron = fragmentedTriangles.Item1;
+        } 
+        return (vertices, triangles);
+    }
 
-        _mesh.Clear();
-        _mesh.vertices = pointOnOctahedron;
-        _mesh.triangles = fragmentedTriangles.Item2;
+    public void ConstructMesh()
+    {
+        Vector3[] pointOnUnitSphere;
+        int[] triangles;
+        if (!_cubeMesh)
+        {
+            (Vector3[], int[]) fragmentedTriangles = TriangleFragmentation(_vertices, _resolution);
+            Vector3[] pointOnOctahedron = fragmentedTriangles.Item1;
+            triangles = fragmentedTriangles.Item2;
+            pointOnUnitSphere = pointOnOctahedron;
+            for (int i = 0; i < pointOnUnitSphere.Length; i++)
+            {
+                pointOnUnitSphere[i] = pointOnUnitSphere[i].normalized;
+            }
+        }
+        else
+        {
+            pointOnUnitSphere = ConstructCubeMesh().Item1;
+            triangles = ConstructCubeMesh().Item2;
+        }
+
+    _mesh.Clear();
+        _mesh.vertices = pointOnUnitSphere;
+        _mesh.triangles = triangles;
         _mesh.RecalculateNormals();
     }
 }
