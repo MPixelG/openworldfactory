f5f26dd72e9aaa0f822ce946edb0df2a66d65c5b
RiddleNeon <julian@scherschmidt.de>
2026-06-14 12:21:11 +0200

    some morton code helpers using bit level calculations

---
+++ Assets/_Project/World/Planet/Scripts/Chunking/OctreeChunkSystem/v2/MortonCodeHelper.cs
@@ -0,0 +1,99 @@
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
+
+namespace _Project.World.Planet.Scripts.Chunking.OctreeChunkSystem.v2
+{
+    public static class MortonCodeHelper
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static ulong ExpandBits(uint v)
+        {
+            ulong x = v & 0x1FFFFFul; // only use the lowest 21 bits, since we can only encode up to 2^21 - 1 in each axis
+            x = (x | (x << 32)) & 0x1F00000000FFFFul;
+            x = (x | (x << 16)) & 0x1F0000FF0000FFul;
+            x = (x | (x << 8)) & 0x100F00F00F00F00Ful;
+            x = (x | (x << 4)) & 0x10C30C30C30C30C3ul;
+            x = (x | (x << 2)) & 0x1249249249249249ul;
+            return x;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static uint CompactBits(ulong x)
+        {
+            x &= 0x1249249249249249ul;
+            x = (x ^ (x >> 2)) & 0x10C30C30C30C30C3ul;
+            x = (x ^ (x >> 4)) & 0x100F00F00F00F00Ful;
+            x = (x ^ (x >> 8)) & 0x1F0000FF0000FFul;
+            x = (x ^ (x >> 16)) & 0x1F00000000FFFFul;
+            x = (x ^ (x >> 32)) & 0x1FFFFFul;
+            return (uint)x;
+        }
+        
+
+        /// <summary>
+        /// returns the morton code for the given 3d coordinate and depth. the coordinate should be in the range of [0, 2^depth) for each axis, where depth is the maximum depth of the octree.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong Encode(int3 coord, byte depth)
+        {
+            // we can encode the 3d coordinate into a morton code by interleaving the bits of the x, y and z coordinates
+            ulong morton = ExpandBits((uint)coord.x) |
+                           (ExpandBits((uint)coord.y) << 1) |
+                           (ExpandBits((uint)coord.z) << 2);
+
+            // set the sentinel bit to the left of the last used bit to encode the depth
+            ulong sentinel = 1ul << (depth * 3);
+
+            return morton | sentinel;
+        }
+
+        /// <summary>
+        /// returns a 3d coordinate from that location
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int3 DecodeCoord(this ulong locationCode)
+        {
+            // remove the sentinel bit
+            int highestBitIndex = 63 - math.lzcnt(locationCode);
+            ulong mask = (1ul << highestBitIndex) - 1ul; // a mask that sets all bits below that sentinel bit to 1 and all bits above to 0
+            ulong rawMorton = locationCode & mask;
+
+            // now we can extract the x, y and z coordinates by compacting the bits. since we know that the x bits are at position 0, 3, 6... we can just shift the raw morton code to the right by 0, 1 and 2 bits and then compact the bits to get the original coordinates.
+            return new int3(
+                (int)CompactBits(rawMorton),
+                (int)CompactBits(rawMorton >> 1),
+                (int)CompactBits(rawMorton >> 2)
+            );
+        }
+
+        /// <summary>
+        /// reads the depth of that location extremely efficiently. make sure the location code contains a sentinel bit.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static byte GetDepth(this ulong locationCode)
+        {
+            int highestBitIndex = 63 - math.lzcnt(locationCode); //lzcnt returns the number of leading zeros, so we subtract that from 63 to get the index of the highest set bit (the sentinel bit).
+            return (byte)(highestBitIndex / 3);
+        }
+
+        /// <summary>
+        /// returns the morton code of the child node
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong GetParent(this ulong locationCode)
+        {
+            // by shifting the code 3 bits to the right, we effectively remove the last child index and move the sentinel bit down to the new depth level.
+            return locationCode >> 3;
+        }
+
+        /// <summary>
+        /// adds a new child index and returns the morton code of the child node. the child index is a value between 0 and 7 that indicates which child node it is.
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static ulong AppendChild(this ulong locationCode, byte childIndex)
+        {
+            // make space for the 3 new bits and add the child index. the sentinel bit is already in the correct position since it moves up with every shift.
+            return (locationCode << 3) | childIndex;
+        }
+    }
+}
--- Assets/_Project/World/Planet/Scripts/Chunking/OctreeChunkSystem/v2/OctreeNode.cs
+++ Assets/_Project/World/Planet/Scripts/Chunking/OctreeChunkSystem/v2/OctreeNode.cs
@@ -4,39 +4,51 @@ namespace _Project.World.Planet.Scripts.Chunking.OctreeChunkSystem.v2
 {
     public struct OctreeNode
     {
-        public ulong MortonCode; // the morton code of the node is a unique identifier for the node that encodes its position in the octree. it is calculated by using 3 bits per layer.
-                                 // since every node has 8 child nodes, we can use 3 bits to encode which child node it is. so the first 3 bits of the morton code would indicate which child node
-                                 // of the root node it is, the next 3 bits would indicate which child node of that node it is and so on.
-                                 // this way we can easily calculate the morton code of any node by just shifting the bits and adding the child index at each layer. 
-                                 // for example, you could represent a morton code of 0b010110011101 as 2535, indicating that its child index 2, then 5, then 3 and then 5 of the root node.
-                                 // and theres another problem that might occur. since hashing (and thus the use in a hash map as a key for example) is way faster if only one ulong is used we need to also store the depth in that morton code
-                                 // however theres a problem. for the computer 0b000000 and 0b000000000 is the same, even though the second one has a deeper depth.
-                                 // we can resolve that problem by adding a trailing 1 at the end of our code. so it would be 0b0000001 and 0000000001, so the computer knows the difference. 
+        /// <summary>
+        /// the morton code of the node is a unique identifier for the node that encodes its position in the octree. it is calculated by using 3 bits per layer.
+        /// since every node has 8 child nodes, we can use 3 bits to encode which child node it is. so the first 3 bits of the morton code would indicate which child node
+        /// of the root node it is, the next 3 bits would indicate which child node of that node it is and so on.
+        /// this way we can easily calculate the morton code of any node by just shifting the bits and adding the child index at each layer. 
+        /// for example, you could represent a morton code of 0b010110011101 as 2535, indicating that its child index 2, then 5, then 3 and then 5 of the root node.
+        /// and theres another problem that might occur. since hashing (and thus the use in a hash map as a key for example) is way faster if only one ulong is used we need to also store the depth in that morton code
+        /// however theres a problem. for the computer 0b000000 and 0b000000000 is the same, even though the second one has a deeper depth.
+        /// we can resolve that problem by adding a leading 1 (sentinel bit) at the end of our code. so it would be 0b0000001 and 0000000001, so the computer knows the difference. 
+        /// </summary>
+        public ulong MortonCode; 
 
 
-        public int
-            FirstChildIndex; // since the nodes are stored linear in one big list on one layer we can store the index of the first child
+        /// <summary>
+        /// since the nodes are stored linearly in one big list on one layer we can store the index of the first child
+        /// </summary>
+        public int FirstChildIndex;
 
-        public byte ChildMask; // an octree node can have up to 8 children. every bit of this byte represents one child.
-                            // so 0b01010001 would indicate that that node has a bottom left front, top left front and top left back
-                            // child, but no other children. this is how i chose to count the corners:
-                            // 
-                            //    6--------7
-                            //   /|       /|
-                            //  / |      / |
-                            // 4--------5  |
-                            // |  |     |  |
-                            // |  2-----|--3
-                            // | /      | /
-                            // |/       |/
-                            // 0--------1
+        /// <summary>
+        /// an octree node can have up to 8 children. every bit of this byte represents one child.
+        /// so 0b01010001 would indicate that that node has a bottom left front, top left front and top left back
+        /// child, but no other children. this is how i chose to count the corners:
+        /// <code>
+        ///    6--------7
+        ///   /|       /|
+        ///  / |      / |
+        /// 4--------5  |
+        /// |  |     |  |
+        /// |  2-----|--3
+        /// | /      | /
+        /// |/       |/
+        /// 0--------1
+        /// </code>
+        /// </summary>
+        public byte ChildMask; 
 
-        public OctreeNodeState State; // the state of the node is either empty, full, mixed or unknown.
-                                    // if its empty, it means that the density field is below the isolevel everywhere in that node.
-                                    // if its full, it means that the density field is above the isolevel everywhere in that node.
-                                    // if its mixed, it means that the density field is above the isolevel in some places and below it in others.
-                                    // and if its unknown, it means that we haven't sampled the density field for that node yet. 
-                                    // this way we can easily skip over completely empty or completely full density fields when we mesh it since theres only a mesh if the isosurface slices through that chunk
+        /// <summary>
+        /// the state of the node is either empty, full, mixed or unknown.
+        /// if its empty, it means that the density field is below the isolevel everywhere in that node.
+        /// if its full, it means that the density field is above the isolevel everywhere in that node.
+        /// if its mixed, it means that the density field is above the isolevel in some places and below it in others.
+        /// and if its unknown, it means that we haven't sampled the density field for that node yet. 
+        /// this way we can easily skip over completely empty or completely full density fields when we mesh it since theres only a mesh if the isosurface slices through that chunk
+        /// </summary>
+        public OctreeNodeState State;
     }
 
     public enum OctreeNodeState : byte
