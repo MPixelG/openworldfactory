7601d814d526759530954123aa8d4102a3bf734f
RiddleNeon <RiddleNeon@users.noreply.github.com>
2026-08-24 15:30:25 +0000

    deploy: de7ed17a7890a979976c35d681d20fc1806830c3

--- search/variables_e.js
+++ search/variables_e.js
@@ -1,8 +1,7 @@
 var searchData=
 [
-  ['oceanradius_0',['OceanRadius',['../class__Project_1_1World_1_1Atmosphere_1_1Shader_1_1AtmosphereSettings.html#a46d0cde3df0787795bb2c04129e50e41',1,'_Project::World::Atmosphere::Shader::AtmosphereSettings']]],
-  ['octreeready_1',['OctreeReady',['../class__Project_1_1World_1_1Planet_1_1Scripts_1_1Chunking_1_1OctreeChunkSystem_1_1PlanetManager.html#ab62d302282c93084937e82a0c56487b1',1,'_Project::World::Planet::Scripts::Chunking::OctreeChunkSystem::PlanetManager']]],
-  ['opticaldepth_2',['opticalDepth',['../class__Project_1_1World_1_1Atmosphere_1_1Shader_1_1AtmospherePass_1_1FullscreenPassData.html#a3ee768aab4c4d0d324cf4ea865bbd374',1,'_Project::World::Atmosphere::Shader::AtmospherePass::FullscreenPassData']]],
-  ['opticaldepthcompute_3',['opticalDepthCompute',['../class__Project_1_1World_1_1Atmosphere_1_1Shader_1_1AtmosphereSettings.html#aed5d3bb208e7f56b4043fbeaeb274c93',1,'_Project::World::Atmosphere::Shader::AtmosphereSettings']]],
-  ['origin_4',['origin',['../struct__Project_1_1World_1_1Planet_1_1Scripts_1_1Chunking_1_1OctreeChunkSystem_1_1Unity_1_1PlanetConfig.html#a303c81faadfd4f66151b881ac405fc0a',1,'_Project::World::Planet::Scripts::Chunking::OctreeChunkSystem::Unity::PlanetConfig']]]
+  ['octreeready_0',['OctreeReady',['../class__Project_1_1World_1_1Planet_1_1Scripts_1_1Chunking_1_1OctreeChunkSystem_1_1PlanetManager.html#ab62d302282c93084937e82a0c56487b1',1,'_Project::World::Planet::Scripts::Chunking::OctreeChunkSystem::PlanetManager']]],
+  ['opticaldepth_1',['opticalDepth',['../class__Project_1_1World_1_1Atmosphere_1_1Shader_1_1AtmospherePass_1_1FullscreenPassData.html#a3ee768aab4c4d0d324cf4ea865bbd374',1,'_Project::World::Atmosphere::Shader::AtmospherePass::FullscreenPassData']]],
+  ['opticaldepthcompute_2',['opticalDepthCompute',['../class__Project_1_1World_1_1Atmosphere_1_1Shader_1_1AtmosphereSettings.html#aed5d3bb208e7f56b4043fbeaeb274c93',1,'_Project::World::Atmosphere::Shader::AtmosphereSettings']]],
+  ['origin_3',['origin',['../struct__Project_1_1World_1_1Planet_1_1Scripts_1_1Chunking_1_1OctreeChunkSystem_1_1Unity_1_1PlanetConfig.html#a303c81faadfd4f66151b881ac405fc0a',1,'_Project::World::Planet::Scripts::Chunking::OctreeChunkSystem::Unity::PlanetConfig']]]
 ];
